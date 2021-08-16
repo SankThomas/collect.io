7228bff8edfe95294e6a52a71825ba2002142308
Thomas Sankara <thomassankara74@gmail.com>
2021-08-16 09:44:29 +0300

    primary buttons

---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+export default function Buttons() {
+  return (
+    <>
+      <article>
+        <button className="btn btn-red mr-3 md:mr-5">Create Invoice</button>
+        <button className="btn btn-green mr-3 md:mr-5">Budget Manager</button>
+        <button className="btn btn-indigo">Todo List</button>
+      </article>
+    </>
+  )
+}
