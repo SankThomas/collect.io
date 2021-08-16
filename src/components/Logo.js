adcc0226c82830212514f6c94b91db5d67766355
Thomas Sankara <thomassankara74@gmail.com>
2021-08-16 09:44:59 +0300

    Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export default function Logo() {
+  return (
+    <>
+      {/* <Link to="/"> */}
+      <h2 className="text-2xl">
+        Collect<span className="text-blue-800">.io</span>
+      </h2>
+      {/* </Link> */}
+    </>
+  )
+}
