4134a762448a9b57f826e6833b7a5d7d5d58a6d5
Thomas Sankara <thomassankara74@gmail.com>
2021-08-16 09:44:17 +0300

    background 'image'

---
+++ src/components/BgImage.js
@@ -0,0 +1,9 @@
+export default function BgImage() {
+  return (
+    <>
+      <h3 className="bg__image__header opacity-20">
+        Collect<span className="text-blue-800 opacity-100">.io</span>
+      </h3>
+    </>
+  )
+}
